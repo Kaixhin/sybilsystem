80fb00a61ea3a3b5a39f191cdb95846afbbcf79c
Kaixhin <design@kaixhin.com>
2015-01-21 12:51:55 +0000

    Edit comment for clarification

--- layers/softmax.m
+++ layers/softmax.m
@@ -2,6 +2,6 @@ function condProb = softmax(z)
   % SOFTMAX Calculates the softmax (normalized exponential) function
   % z:        Input
   % condProb: Conditional probabilities
-  condProb = exp(bsxfun(@minus, z, max(z, [], 1))); % Prevent overflow
+  condProb = exp(bsxfun(@minus, z, max(z, [], 1))); % Prevent over/underflow
   condProb = bsxfun(@rdivide, condProb, sum(condProb)); % Normalise for probabilities
 end
